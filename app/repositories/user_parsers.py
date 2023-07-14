5e1c38cb5f6aa8877943811ce4b71c415aad94f4
Denis Grishankov <66868270+XOBBIT1@users.noreply.github.com>
2023-07-14 12:56:30 +0300

    checking type

--- app/repositories/user_parsers.py
+++ app/repositories/user_parsers.py
@@ -36,8 +36,8 @@ class UserParsersRepository(BaseRepository):
     async def delete_by_id(self, owner_id: str, _id: Union[str, ObjectId]):
         try:
             user_id = ObjectId(_id) if isinstance(_id, str) else _id
-            return await self.db[self.collection]\
-                .delete_one({"owner_id": owner_id, "_id": str(user_id)})  # type: ignore
+            return await self.db[self.__str__()]\
+                .delete_one({"owner_id": owner_id, "_id": user_id})  # type: ignore
         except errors.InvalidId:
             return None
 
